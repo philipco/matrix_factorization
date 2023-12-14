1a60e442b00590c726d6931d5fd85519fb6c9850
Constantin Philippenko <constantin.philippenko@polytechnique.edu>
2023-12-14 14:23:20 +0100

    Plot improved.

--- src/main.py
+++ src/main.py
@@ -17,8 +17,8 @@ matplotlib.rcParams.update({
     'text.latex.preamble': r'\usepackage{amsfonts}'
 })
 
-NB_EPOCHS = 5000
-NB_CLIENTS = 10
+NB_EPOCHS = 2500
+NB_CLIENTS = 1
 
 FONTSIZE=9
 
@@ -27,67 +27,27 @@ if __name__ == '__main__':
 
     network = Network(NB_CLIENTS, 100, 100, 5, 6)
 
-    # GD on U, V.
-    randomAlternateGD = AlternateGD(network, NB_EPOCHS, 0.01, "RANDOM")
-    error_random_alternate = randomAlternateGD.gradient_descent()
-    alternateGD = AlternateGD(network, NB_EPOCHS, 0.01, "SMART")
-    error_alternate = alternateGD.gradient_descent()
-    bismart_alternateGD = AlternateGD(network, NB_EPOCHS, 0.01, "BI_SMART")
-    bismart_error_alternate = bismart_alternateGD.gradient_descent()
+    optimizations = {"UV": AlternateGD, "V": GD_ON_V, "U": GD_ON_U}
+    errors = {"UV": {}, "V": {}, "U": {}}
+    inits = ["RANDOM", "SMART", "BI_SMART", "ORTHO"]
 
-    # GD on V
-    algoV = GD_ON_V(network, NB_EPOCHS, 0.01, "SMART")
-    errorV = algoV.gradient_descent()
-    random_algoV = GD_ON_V(network, NB_EPOCHS, 0.01, "RANDOM")
-    random_errorV = random_algoV.gradient_descent()
-    bismart_algoV = GD_ON_V(network, NB_EPOCHS, 0.01, "BI_SMART")
-    bismart_errorV = bismart_algoV.gradient_descent()
-    ortho_algoV = GD_ON_V(network, NB_EPOCHS, 0.01, "ORTHO")
-    ortho_errorV = ortho_algoV.gradient_descent()
-
-    # GD on U
-    algoU = GD_ON_U(network, NB_EPOCHS, 0.01, "SMART_FOR_GD_ON_U")
-    errorU = algoU.gradient_descent()
-    random_algoU = GD_ON_U(network, NB_EPOCHS, 0.01, "RANDOM")
-    random_errorU = random_algoU.gradient_descent()
-    bismart_algoU = GD_ON_U(network, NB_EPOCHS, 0.01, "BI_SMART_FOR_GD_ON_U")
-    bismart_errorU = bismart_algoU.gradient_descent()
-    ortho_algoU = GD_ON_U(network, NB_EPOCHS, 0.01, "ORTHO_FOR_GD_ON_U")
-    ortho_errorU = ortho_algoU.gradient_descent()
-
-
-
-    print(f"\n{algoU.name()}:")
-    print("First value of F:", errorU[0])
-    print("Last value of F:", errorU[-1])
-
-    print(f"\n{algoV.name()}:")
-    print("First value of F:", errorV[0])
-    print("Last value of F:", errorV[-1])
-
-    print(f"\n{randomAlternateGD.name()}:")
-    print("First value of F:", error_random_alternate[0])
-    print("Last value of F:", error_random_alternate[-1])
-
-    print(f"\n{alternateGD.name()}:")
-    print("First value of F:", error_alternate[0])
-    print("Last value of F:", error_alternate[-1])
+    for key in optimizations.keys():
+        print(f"=== {key} ===")
+        for init in inits:
+            algo = optimizations[key](network, NB_EPOCHS, 0.01, init)
+            errors[key][init] = algo.gradient_descent()
+            print(f"{init}\terror min:", errors[key][init][-1])
 
     COLORS = ["tab:blue", "tab:orange", "tab:green", "tab:red", "tab:purple", "tab:brown", "tab:cyan"]
 
+    optim_colors = {"UV": COLORS[0], "V": COLORS[1], "U": COLORS[2]}
+    init_linestyle = {"RANDOM": "-.", "SMART": "-", "BI_SMART": "--", "ORTHO": ":"}
+
 
     fig, axs = plt.subplots(1, 1, figsize=(6, 4))
-    axs.plot(np.log10(error_random_alternate), label="gd on U, V, random init.", color=COLORS[0], linestyle="-.")
-    axs.plot(np.log10(error_alternate), label="gd on U, V, smart init.", color=COLORS[0])
-    axs.plot(np.log10(bismart_error_alternate), label="gd on U, V, bismart init.", color=COLORS[0], linestyle="--")
-    axs.plot(np.log10(random_errorV), label="gd on V, random init.", color=COLORS[1], linestyle="-.")
-    axs.plot(np.log10(errorV), label="gd on V, smart init.".format(algoV.smallest_sv[1]), color=COLORS[1]) #, sv(V)={:.2f}
-    axs.plot(np.log10(bismart_errorV), label="gd on V, bismart init.", color=COLORS[1], linestyle="--")
-    axs.plot(np.log10(ortho_errorV), label=f"gd on V, ortho init.", color=COLORS[1], linestyle=":")
-    axs.plot(np.log10(random_errorU), label="gd on U, random init.", color=COLORS[2], linestyle="-.")
-    axs.plot(np.log10(errorU), label="gd on U, smart init.".format(algoU.smallest_sv[0]), color=COLORS[2]) #, sv(U)={:.2f}
-    axs.plot(np.log10(bismart_errorU), label="gd on U, bismart init.", color=COLORS[2], linestyle="--")
-    axs.plot(np.log10(ortho_errorU), label=f"gd on U, ortho init.", color=COLORS[2], linestyle=":")
+    for key in optimizations.keys():
+        for init in inits:
+            axs.plot(np.log10(errors[key][init]), color=optim_colors[key], linestyle=init_linestyle[init])
 
     gd_legend = [Line2D([0], [0], color=COLORS[0], lw=2, label='gd on U, V'),
                    Line2D([0], [0], color=COLORS[1], lw=2, label='gd on V'),
