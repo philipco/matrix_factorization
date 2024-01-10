e496ce34ea7d5dcd0415eadd93936b6e2edbbe20
Constantin Philippenko <constantin.philippenko@polytechnique.edu>
2024-01-10 19:26:44 +0100

    Random initialization as in Ye and Du, Neurips 2021. Draft for eigenvalues initialization from Jain et al 2012.

--- src/MFInitialization.py
+++ src/MFInitialization.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.linalg import eigh
 from scipy.sparse.linalg import svds
 from scipy.stats import ortho_group
 
@@ -18,13 +19,19 @@ def generate_gaussian_matrix(n, d, std=1):
 
 
 def random_MF_initialization(network: Network):
+    """Implementation of Global Convergence of Gradient Descent for Asymmetric Low-Rank Matrix Factorization, Ye and Du,
+    Neurips 2021"""
     plunging_dimension = network.plunging_dimension
-    step_size = 1
+    S = np.concatenate([client.S for client in network.clients])
+    smallest_eigenvalues = svds(S, k=network.plunging_dimension - 1, which='SM')[1]
+    sigma_min = np.min(smallest_eigenvalues[np.nonzero(smallest_eigenvalues)])  # smallest non-zero eigenvalue
+    sigma_max = svds(S, k=1, which='LM')[1][0]
+    std = sigma_min / (np.sqrt(sigma_max * plunging_dimension **3 ) * (network.dim + network.nb_samples))
     for client in network.clients:
-        client.set_U(generate_gaussian_matrix(network.nb_samples, plunging_dimension, 1) / (
-            np.sqrt(plunging_dimension)))
-        client.set_V(generate_gaussian_matrix(network.dim, plunging_dimension, 1) / (
-            np.sqrt(plunging_dimension)))
+        client.set_U(generate_gaussian_matrix(network.nb_samples, plunging_dimension, std))
+        client.set_V(generate_gaussian_matrix(network.dim, plunging_dimension, std))
+    print(f"===> kappa: {sigma_max / sigma_min}")
+    return sigma_min, sigma_max
 
 
 def smart_MF_initialization(network: Network):
@@ -105,6 +112,7 @@ def ortho_MF_initialization(network: Network):
     smallest_eigenvalues = svds(U, k=network.plunging_dimension - 1, which='SM')[1]
     sigma_min = np.min(smallest_eigenvalues[np.nonzero(smallest_eigenvalues)])  # smallest non-zero eigenvalue
     sigma_max = svds(U, k=1, which='LM')[1][0]
+    print(f"===> kappa: {sigma_max / sigma_min}")
     return sigma_min, sigma_max
 
 def ortho_MF_initialization_for_GD_on_U(network: Network):
@@ -122,6 +130,43 @@ def ortho_MF_initialization_for_GD_on_U(network: Network):
     print(f"===> kappa: {sigma_max / sigma_min}")
     return sigma_min, sigma_max
 
+
+def eig_MF_initialization(network: Network):
+    U = []
+    # When initialization U in the span of S, Phi is shared by all clients.
+    # Phi_U = generate_gaussian_matrix(network.dim, network.plunging_dimension, 1)
+    for client in network.clients:
+        w, v = eigh(client.S, subset_by_index=[0, network.plunging_dimension-1])
+        client.set_U(v)
+        U.append(client.U)
+        Phi_V = generate_gaussian_matrix(network.dim, network.plunging_dimension, 1 / np.sqrt(network.dim))
+        client.set_V(Phi_V)
+    U = np.concatenate(U)
+    smallest_eigenvalues = svds(U, k=network.plunging_dimension - 1, which='SM')[1]
+    sigma_min = np.min(smallest_eigenvalues[np.nonzero(smallest_eigenvalues)])  # smallest non-zero eigenvalue
+    sigma_max = svds(U, k=1, which='LM')[1][0]
+    std = sigma_min / (np.sqrt(sigma_max * network.plunging_dimension ** 3) * (network.dim + network.nb_samples))
+    # for client in network.clients:
+    #     client.U /= std**2
+    #     client.V /= std ** 2
+    print(f"===> kappa: {sigma_max / sigma_min}")
+    return sigma_min, sigma_max
+
+
+def eig_MF_initialization_for_GD_on_U(network: Network):
+    V = np.zeros((network.dim, network.plunging_dimension))
+    for client in network.clients:
+        w, v = eigh(client.S, subset_by_index=[0, network.plunging_dimension-1])
+        V += v
+        Phi_U = generate_gaussian_matrix(network.dim, network.plunging_dimension, 1 / np.sqrt(network.dim))
+        client.set_U(Phi_U)
+    for client in network.clients:
+        client.set_V(V)
+    smallest_eigenvalues = svds(V, k=network.plunging_dimension - 1, which='SM')[1]
+    sigma_min = np.min(smallest_eigenvalues[np.nonzero(smallest_eigenvalues)])  # smallest non-zero eigenvalue
+    sigma_max = svds(V, k=1, which='LM')[1][0]
+    print(f"===> kappa: {sigma_max / sigma_min}")
+    return sigma_min, sigma_max
 
 def smart_sparse_MF_initialization(network: Network):
     sigma_min = 0
