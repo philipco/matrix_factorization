1e605faf580483b780b07795ee05af9cabcf55fb
Constantin Philippenko <cphilippenko@free.fr>
2024-11-07 17:00:33 +0100

    Run algo until reach given precision.

--- src/algo/AbstractAlgorithm.py
+++ src/algo/AbstractAlgorithm.py
@@ -12,16 +12,12 @@ from src.Network import Network
 class AbstractAlgorithm(ABC):
     """Abstract class to define any algorithm to do matrix factorisation."""
 
-    def __init__(self, network: Network, nb_epoch: int, init_type: str) -> None:
+    def __init__(self, network: Network, nb_epoch: int) -> None:
         self.network = network
-        self.init_type = init_type
         self.nb_clients = network.nb_clients
         self.nb_epoch = nb_epoch
 
         self.errors = []
-
-        # STEP-SIZE
-        self.__initialization__()
 
         # REGULARIZATION
         self.l1_coef = 0
@@ -47,9 +43,19 @@ class AbstractAlgorithm(ABC):
         """Global objective function that we want to minimize."""
         return np.sum([client.loss(client.U, client.V, self.l1_coef, self.l2_coef, self.nuc_coef) for client in self.network.clients])
 
-    def run(self):
+    def run(self, eps=None):
         """Run the algorithm of matrix factorisation."""
         self.errors.append(self.__F__())
-        for i in range(self.nb_epoch):
-            self.__epoch_update__(i)
+        if eps is None:
+            for i in range(self.nb_epoch):
+                self.__epoch_update__(i)
+        else:
+            i = 0
+            while self.errors[-1] > eps and i < 1000:
+                self.__epoch_update__(i)
+                i+=1
+            if i == 1000:
+                print("Warning: requires more than 1000 iterations.")
+            else:
+                print(f"Number of iterations: {i}")
         return self.errors
